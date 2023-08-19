75123f29d53554ffb04c1e8b9047c4e0200c54c3
chloeadriancreates <chloeadriancreates@gmail.com>
2023-08-19 19:41:40 +0200

    Update project urls + email

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
     const [emailLabel, setEmailLabel] = useState("Copy my email address");
 
     const copyEmail = () => {
-        navigator.clipboard.writeText("hi@chloeadrian.dev");
+        navigator.clipboard.writeText("hello@chloeadrian.dev");
         if(language === "fr") {
             setEmailLabel("Adresse mail copiée !");
         } else {
